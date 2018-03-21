49a5ad722aab46981b3dd2a15d7f8c5076deb9ca
Elena Ingraham <elena.ingraham@colorado.edu>
2018-03-20 20:57:25 -0600

    Beginning of the Shopping Cart classes. Mostly psuedocode right now.

---
+++ breakingbad/src/shoppingCart.java
@@ -0,0 +1,8 @@
+public class shoppingCart{
+    shoppingCart HashMap<String == productName, int == price> = new HashMap;
+    //if statement
+    //if in stock add to cart, add item and price to hashmap
+    //display message 'Item successfully added to cart
+    //else display error
+    //create 'total' variable, the sum of th price of all the items in the cart
+}
