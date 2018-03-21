7660bc02a0f84a9b422519e5e39cdc9cc77775eb
fram6578 <fram6578@colorado.edu>
2018-03-20 18:40:49 -0600

    Updated Deal class
    Encapsulated date range
    Refactored smelly code

--- breakingbad/src/Deal.java
+++ breakingbad/src/Deal.java
@@ -1,4 +1,3 @@
-package BreakingBread;
 import java.util.Date;
 
 abstract public class Deal {
@@ -6,26 +5,73 @@ abstract public class Deal {
     TODO
     Need deal activators i.e,
     Buy X amount of items results in discount
+    Need destructor handler, i.e. when range ends
      */
+    protected class DateRange {
+        private Date start,end;
+
+        public DateRange(){
+            setStart(new Date(0));
+            setEnd(new Date(0));
+        }
+
+        public DateRange(Date start, Date end){
+            setStart(start);
+            setEnd(end);
+        }
+
+        public Date getStart() {
+            return start;
+        }
+
+        public Date getEnd() {
+            return end;
+        }
+
+        public void setStart(Date start) {
+            if(getEnd().before(getStart()))
+                //Throw exception if end is before beginning but not if they're at the same time
+                throw new IllegalArgumentException("Start date must be before end date");
+            else
+                this.start = start;
+        }
+
+        public void setEnd(Date end) {
+            if(getEnd().before(getStart()))
+                //Throw exception if end is before beginning but not if they're at the same time
+                throw new IllegalArgumentException("Start date must be before end date");
+            else if(!getEnd().equals(getStart()) && getEnd().before(new Date()))
+                throw new IllegalArgumentException("End date must be after current date");
+            else
+                this.end = end;
+        }
+
+        public boolean inRange(Date check){
+            if(getEnd().equals(getStart()))
+                //Infinite time is represented by equal start and end
+                return true;
+            else if(getEnd().before(check) || getStart().after(check))
+                return false;
+            else
+                //If they're not equal and check doesn't fall outside the range it's in range
+                return true;
+        }
+
+    }
 
     private int id;
     private String name, description;
-    private Date start,end;
+    private DateRange range;
     private float amountOff;
 
     abstract public float discount(float price);
 
-    public Deal() {
-        //TODO
-    }
-
-    public Deal(int id, String name, String description, Date start, Date end, float amountOff) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.start = start;
-        this.end = end;
-        this.amountOff = amountOff;
+    public Deal(int id, String name, String description, DateRange range, float amountOff) {
+        setId(id);
+        setName(name);
+        setDescription(description);
+        setRange(range);
+        setAmountOff(amountOff);
     }
 
     public int getId() {
@@ -40,12 +86,8 @@ abstract public class Deal {
         return description;
     }
 
-    public Date getStart() {
-        return start;
-    }
-
-    public Date getEnd() {
-        return end;
+    public DateRange getRange() {
+        return range;
     }
 
     public float getAmountOff() {
@@ -64,30 +106,45 @@ abstract public class Deal {
         this.description = description;
     }
 
-    public void setStart(Date start) {
-        this.start = start;
+    public void setRange(DateRange range) {
+        this.range = range;
     }
 
-    public void setEnd(Date end) {
-        this.end = end;
-    }
-
-    public void setAmountOff(float amountOff) {
-        this.amountOff = amountOff;
+    public void setAmountOff(float amountOff){
+        if(getAmountOff() > 0)
+            this.amountOff = amountOff;
+        else
+            throw new IllegalArgumentException("Deal must have more than 0 off");
     }
 }
 
 //Should this really have it's own file?
 class FixedDeal extends Deal {
+    public FixedDeal(int id, String name, String description, Deal.DateRange range, float amountOff) {
+        super(id, name, description, range, amountOff);
+    }
+
     @Override
     public float discount(float price) {
-        return price - this.getAmountOff();
+        return price - getAmountOff();
     }
 }
 
 class PercentDeal extends Deal {
+    public PercentDeal(int id, String name, String description, DateRange range, float amountOff) {
+        super(id, name, description, range, amountOff);
+    }
+
+    @Override
+    public void setAmountOff(float amountOff) {
+        if(amountOff <= 1)
+            super.setAmountOff(amountOff);
+        else
+            throw new IllegalArgumentException("Deal must be between 0% and 100% off");
+    }
+
     @Override
     public float discount(float price) {
-        return price * this.getAmountOff();
+        return price * getAmountOff();
     }
 }
