f3f82d60239a467ba2587753f2b5224e6446b6b7
Elena Ingraham <elena.ingraham@colorado.edu>
2018-03-21 20:06:17 -0600

    New Methods, still a skeleton but now with more bones

--- breakingbad/src/shoppingCart.java
+++ breakingbad/src/shoppingCart.java
@@ -1,5 +1,21 @@
+Map<String === productName ,  int price>
+Map<String, price> cart = new HashMap<String, price>();
 public class shoppingCart{
-    shoppingCart HashMap<String == productName, int == price> = new HashMap;
+    void private setCart{
+
+    }
+    Hashmap public getCart{
+
+    }
+    void private setTotal{
+
+    }
+    int public getTotal{
+        
+    }
+    //cart is our Hashmap
+    //Display all items in hashmap when in cart page
+    //shoppingCart HashMap<String == productName, int == price> = new HashMap;
     //if statement
     //if in stock add to cart, add item and price to hashmap
     //display message 'Item successfully added to cart
