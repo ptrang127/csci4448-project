919b58d38c609e30511c5fbb0650584f2565c789
Elena Ingraham <elena.ingraham@colorado.edu>
2018-03-21 23:32:18 -0600

    More detail added to the setCart method. I will have to work closely with the UI classes to get the correct buttons etc

--- breakingbad/src/shoppingCart.java
+++ breakingbad/src/shoppingCart.java
@@ -1,7 +1,17 @@
 Map<String === productName ,  int price>
-Map<String, price> cart = new HashMap<String, price>();
+Map<String, int, int > cart = new HashMap();
 public class shoppingCart{
+    //if add to cart = y
+    //;
     void private setCart{
+        if(addToCart()){
+            cart.put(product.name, product.price, product.quantity);
+            total = product.price * quantity;
+            System.out.println("Item Successfully Added to Cart");
+            //possible addition of continue shopping or checkout on display
+
+        }
+
 
     }
     Hashmap public getCart{
@@ -11,14 +21,15 @@ public class shoppingCart{
 
     }
     int public getTotal{
-        
+
     }
     //cart is our Hashmap
     //Display all items in hashmap when in cart page
-    //shoppingCart HashMap<String == productName, int == price> = new HashMap;
+    //there should also be a quantity variable
     //if statement
     //if in stock add to cart, add item and price to hashmap
     //display message 'Item successfully added to cart
     //else display error
     //create 'total' variable, the sum of th price of all the items in the cart
+    //display menu to continue shopping(optional)
 }
