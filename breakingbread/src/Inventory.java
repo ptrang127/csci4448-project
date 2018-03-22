71c2f5e633ee32a7aabc1ccf26b93cb00c11b11c
ptrang127 <p.trang127@gmail.com>
2018-03-21 23:27:18 -0600

    added inventory.java

---
+++ breakingbread/src/Inventory.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Inventory {
+
+    private ArrayList<Product> stock = new ArrayList<Product>();
+
+    public int addProduct(Product newProduct) {
+
+    }
+
+    public int removeProduct(int oldID) {
+
+    }
+
+    public int changeProduct(int oldID, Product newProduct) {
+
+    }
+
+    public ArrayList<Integer> searchProduct(String query) {
+
+    }
+
+}
