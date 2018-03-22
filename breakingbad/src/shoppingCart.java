480af18fd8a6c9bbb197288296f83ed0732efd28
Elena Ingraham <elena.ingraham@colorado.edu>
2018-03-21 23:41:49 -0600

    A few more important additions. I will review iterating through a hashmap to display the cart, with the item's price and quantity. And I will use another iterator to keep track of the total cost variable. The skeleton is coming together.

--- breakingbad/src/shoppingCart.java
+++ breakingbad/src/shoppingCart.java
@@ -1,26 +1,33 @@
-Map<String === productName ,  int price>
-Map<String, int, int > cart = new HashMap();
 public class shoppingCart{
     //if add to cart = y
     //;
+    Map<String, int, int > cart = new HashMap();
+    iterator i = set.iterator
     void private setCart{
         if(addToCart()){
             cart.put(product.name, product.price, product.quantity);
-            total = product.price * quantity;
             System.out.println("Item Successfully Added to Cart");
             //possible addition of continue shopping or checkout on display
-
         }
-
-
     }
     Hashmap public getCart{
+        while(i.hasNext()){
+            //something with an interator
+            //to iterate and then print all items in cart
+        }
 
     }
-    void private setTotal{
-
+    void protected setTotal{ //probably needs to be accessed in getTotal method
+        while(i.hasNext()) //possibly create a new iterator
+        {
+            //iterate through all items in hashmap
+            //for every item
+            total += (item*quantity);
+        }
     }
     int public getTotal{
+        System.out.println(total);
+        return total;
 
     }
     //cart is our Hashmap
