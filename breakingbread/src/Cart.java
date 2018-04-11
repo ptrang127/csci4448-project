e6920d33efa2c8d98e29737f3ff5c13135049f11
Elena Ingraham <elena.ingraham@colorado.edu>
2018-04-11 17:17:27 -0600

    Working on Cart class, need to work with iterator to clear cart.

--- breakingbread/src/Cart.java
+++ breakingbread/src/Cart.java
@@ -1,14 +1,21 @@
 import java.util.*; //just to see the colors on intellij 
 public class Cart{
-    //if add to items = y
+    ArrayList<product> Products = new ArrayList<product>();
     public float price = 0;
-    Map<String, Product> items = new HashMap<String, Product>();
-    private void addItem(Product product){
+    public Cart(Product product){
+        Product.add(product);
+    }
+    private static Product[] addItem(Product product){
         items.put(product.getName(), product);
         System.out.println("Item Successfully Added to Cart");
         //possible addition of continue shopping or checkout on display
     }
-
+    private static Product[] removeItem(Product product){
+        items.
+    }
+    private void clearCart(){
+        for
+    }
     public float getPrice() {
         Set currentCart = items.entrySet(); //cool built in Java ish to make a set from the hashmap idk if this should be here though
         Iterator<String> i = currentCart.iterator(); //this should work for the interator, length of the items
