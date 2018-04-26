d0f4e28a8d0390233d97b05c2ccd4af249dae72e
HussainQ <shq_sid@yahoo.com>
2018-04-26 17:11:23 -0600

    Testing Admin functionality

--- breakingbread/src/Admin.java
+++ breakingbread/src/Admin.java
@@ -1,29 +1,37 @@
 import java.util.*;
 
 public class Admin extends User{
-/*
-    public User viewAccount(int userId) {
+
+    Inventory inventory = new Inventory();
+
+   /* public User viewAccount(int userId) {
         // mysql code to print out user
-    }
+
+    }*/
 
     public int removeAccount(int userId){
         //
+        return userId;
     }
 
     public int addProduct(Product newProduct){
         inventory.addProduct(newProduct);
+        return 0;
     }
 
     public int removeProduct(int oldId){
         inventory.removeProduct(oldId);
+        return oldId;
     }
 
     public int changeProduct(int oldId, Product newProduct){
         inventory.changeProduct(oldId, newProduct);
+        return oldId;
     }
 
     public int addDeal(int productId, Deal deal){
-        Product.addDeal(deal);
+        //Product.addDeal(deal);
+        return productId;
     }
-    */
+
 }
