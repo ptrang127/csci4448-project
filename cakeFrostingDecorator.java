e6c3a6a9aeb02f825a10190593434d1a4bea7d69
elin3952 <31673552+elin3952@users.noreply.github.com>
2018-04-29 16:14:11 -0600

    Delete cakeFrostingDecorator.java

--- cakeFrostingDecorator.java
+++
@@ -1,6 +0,0 @@
-public class cakefrostingDecorator{
-    public static void main(String[] args){
-        customProduct = new customCake();
-        customProduct redCake = new frostingColor(new customCake("red"));
-    }
-}
